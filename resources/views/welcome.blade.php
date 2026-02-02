b9ef4fb7dd9dd508b2cffd889e1c5f5c731df0f5
Qerakl <german111474457@gmail.com>
2026-02-02 16:09:49 +0500

    Добавил структуру для фронта

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Главная</title>
-</head>
-<body>
-главная страница
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Главная страница')
+
+@section('content')
+    <p>Это содержимое моей страницы.</p>
+@endsection
