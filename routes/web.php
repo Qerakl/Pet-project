e6b6073eb0c9c0d938212d591bdb3023b98d612d
Qerakl <german111474457@gmail.com>
2026-02-03 16:02:04 +0500

    Вынес в отдельный файл роуты для аутентификации

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['guest'])->group(function () {
-    Route::get('/register', [UserController::class, 'viewRegister'])->name('view.register');
-    Route::post('/register', [UserController::class, 'register'])->name('register');
-
-    Route::get('/login', [UserController::class, 'viewLogin'])->name('view.login');
-    Route::post('/login', [UserController::class, 'login'])->name('login');
-});
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/profile', [UserController::class, 'viewProfile'])->name('view.profile');
-    Route::post('logout', [UserController::class, 'logout'])->name('logout');
-
-    Route::get('/settings', [UserController::class, 'viewSettings'])->name('view.settings');
-    Route::put('/settings/user/update/password', [UserController::class, 'updatePassword'])->name('update.user.password');
-    Route::put('/settings/user/update/{id}', [UserController::class, 'update'])->name('update.user');
-
-});
+require __DIR__.'/web/auth.php';
---
+++ routes/web/auth.php
@@ -0,0 +1,21 @@
+<?php
+use App\Http\Controllers\Auth\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/register', [UserController::class, 'viewRegister'])->name('view.register');
+    Route::post('/register', [UserController::class, 'register'])->name('register');
+
+    Route::get('/login', [UserController::class, 'viewLogin'])->name('view.login');
+    Route::post('/login', [UserController::class, 'login'])->name('login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [UserController::class, 'viewProfile'])->name('view.profile');
+    Route::post('logout', [UserController::class, 'logout'])->name('logout');
+
+    Route::get('/settings', [UserController::class, 'viewSettings'])->name('view.settings');
+    Route::put('/settings/user/update/password', [UserController::class, 'updatePassword'])->name('update.user.password');
+    Route::put('/settings/user/update/{id}', [UserController::class, 'update'])->name('update.user');
+
+});
