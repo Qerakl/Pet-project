63f7b3b167498c899c6374cdde2e23b3335830b7
Qerakl <german111474457@gmail.com>
2026-02-05 12:04:59 +0500

    Добавил обновление комментария и удаление

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Comment\StoreCommentRequest;
+use App\Http\Requests\Comment\UpdateCommentRequest;
 use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -19,5 +20,25 @@ class CommentController extends Controller
         ]);
 
         return redirect()->route('posts.show', ['post' => $post]);
+    }
+
+    public function update(UpdateCommentRequest $request, Post $post, Comment $comment)
+    {
+        $comment->update([
+            'comment' => $request->comment,
+        ]);
+
+        return redirect()->route('posts.show', ['post' => $post]);
+    }
+
+    public function destroy(Post $post, Comment $comment)
+    {
+        if(Auth::id() === $comment->user_id)
+        {
+            $comment->delete();
+            return redirect()->route('posts.show', ['post' => $post]);
+        }
+
+        return response(null, 403);
     }
 }
