5b4041016375556cf41b2f72015d0216d55b446f
Qerakl <german111474457@gmail.com>
2026-02-05 11:54:07 +0500

    Создал сущность комментарии добавил к ней миграцию, модель, фабрику, сидеры и прописал отношения

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,27 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Post extends Model
+class Comment extends Model
 {
-    /** @use HasFactory<\Database\Factories\PostFactory> */
+    /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'title',
-        'body',
+        'post_id',
         'user_id',
+        'comment',
     ];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function comments(): HasMany
-    {
-        return $this->hasMany(Comment::class);
     }
 }
